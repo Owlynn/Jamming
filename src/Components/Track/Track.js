b86b1f4359acc0a127eb801d67b8b3d4c4675029
Owlynn <contact@jessalynn.fr>
2022-10-15 18:41:30 +0200

    correcting bugs

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -30,13 +30,13 @@ class Track extends React.Component{
         return (
             <div class="Track">
                 <div class="Track-information">
-                    <h3><!-- track name will go here --></h3>
-                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                    <h3>TrackName</h3>
+                    <p>Track Artist |TrackAlbum</p>
                 </div>
                 {this.renderAction()}
             </div>
         )     
     }
-       
+}
 
 export default Track;
