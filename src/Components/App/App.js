43fdb091581b8bd46a0f7ebec3960d8e3941cad9
Owlynn <contact@jessalynn.fr>
2022-10-16 11:23:07 +0200

    inserting the components in the placeholders

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,10 +8,10 @@ function App() {
     <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-    {/* <!-- Add a SearchBar component --> */}
+    <SearchBar/>
     <div className="App-playlist">
-      {/* <!-- Add a SearchResults component --> */}
-      {/* <!-- Add a Playlist component --> */}
+      <SearchResults/>
+      <PlayList/>
     </div>
   </div>
 </div>
